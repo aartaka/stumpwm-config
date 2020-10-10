a84ff9541a09e0a4e1d52c86a46ffb10ae868fed
aartaka <aartaka@protonmail.com>
2020-10-11 00:42:54 +0500

    Narrow binwarp keybindings down to a minimal necessary set.
    I've encountered several collisions, especially between arrow keys
    being bound in binwarp and being necessary for something I navigate in
    using binwarp. Making less bindings minimizes these collisions.

--- init.lisp
+++ init.lisp
@@ -53,11 +53,19 @@
   (message (battery-portable::battery-info-string)))
 
 (binwarp:define-binwarp-mode binwarp-mode
-    "m" (:map *root-map*)
-  ((kbd "C-n") "ratrelwarp  0 +5")
-  ((kbd "C-p") "ratrelwarp  0 -5")
-  ((kbd "C-f") "ratrelwarp +5  0")
-  ((kbd "C-b") "ratrelwarp -5  0")
+    "m" (:map *root-map* :redefine-bindings t)
+  ((kbd "n") "binwarp down")
+  ((kbd "p") "binwarp up")
+  ((kbd "f") "binwarp right")
+  ((kbd "b") "binwarp left")
+
+  ((kbd "0") "init-binwarp")
+  ((kbd "TAB") "back-binwarp")
+
+  ((kbd "C-M-n") "ratrelwarp  0 +5")
+  ((kbd "C-M-p") "ratrelwarp  0 -5")
+  ((kbd "C-M-f") "ratrelwarp +5  0")
+  ((kbd "C-M-b") "ratrelwarp -5  0")
 
   ((kbd "RET") "ratclick 1")
   ((kbd "SPC") "ratclick 3"))
