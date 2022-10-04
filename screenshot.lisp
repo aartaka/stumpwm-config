defe4c0b42de1596aad0c41a8028776049a804f1
Artyom Bologov <aartaka@protonmail.com>
2022-10-04 18:13:46 +0400

    screenshot: Ensure directories exist.

--- screenshot.lisp
+++ screenshot.lisp
@@ -2,6 +2,7 @@
 
 (defcommand timestamp-screenshot () ()
   (let ((file-name (format nil (home "/Documents/screenshots/~X.png") (get-universal-time))))
+    (uiop/cl:ensure-directories-exist file-name)
     (screenshot:screenshot-window  file-name)
     (uiop:launch-program (list "gimp" file-name))))
 
