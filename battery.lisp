e42fe467b1f901ac167ee94fb33bcc081961ad01
aartaka <aartaka@protonmail.com>
2022-11-20 21:16:37 +0400

    battery: Better logic for percent parsing.

--- battery.lisp
+++ battery.lisp
@@ -13,7 +13,9 @@
      (loop while t
            do (sleep 20)
            when (and (battery-accessible-p)
-                     (< (parse-integer (remove-if-not #'digit-char-p (battery-portable::battery-info-string)))
+                     (< (parse-integer (first (ppcre:all-matches-as-strings
+                                               "\\d{1,3}%" (battery-portable::battery-info-string)))
+                                       :junk-allowed t)
                         10))
              do (set-border-color "red")
              and do (set-msg-border-width 10)
