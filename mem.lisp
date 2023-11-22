cf240a28f076aaf4ec540a0abf8483826fad538b
Artyom Bologov <aartaka@protonmail.com>
2023-11-22 15:33:51 +0400

    Re-enable and re-style the mode line.

--- mem.lisp
+++ mem.lisp
@@ -1,5 +1,4 @@
 (in-package :stumpwm-user)
 
 (setf mem::*mem-usage-bar-empty* #\Space
-      mem::*mem-modeline-fmt* "^07[^70 RAM%p^07]^70"
-      *screen-mode-line-format* (concat "%M " *screen-mode-line-format*))
+      mem::*mem-modeline-fmt* "RAM%p")
