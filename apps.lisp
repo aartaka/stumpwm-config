d5d37cca45eec2d88753f2a0cde5a9bef49137e7
Artyom Bologov <aartaka@protonmail.com>
2023-12-19 18:03:22 +0400

    apps: Add.

---
+++ apps.lisp
@@ -0,0 +1,17 @@
+(in-package :stumpwm-user)
+
+(defcommand doom () ()
+ (uiop:launch-program (list "chocolate-doom" "-iwad" (home "/Documents/doom/GAME/DOOMU.WAD"))))
+(defcommand doom2 () ()
+  (uiop:launch-program (list "chocolate-doom" "-iwad" (home "/Documents/doom2/DOOM2.WAD"))))
+(defcommand doom-plutonia () ()
+  (uiop:launch-program (list "chocolate-doom" "-iwad" (home "/Documents/doom-plutonia/PLUTONIA.WAD"))))
+(defcommand quake () ()
+  (uiop:launch-program (list "ioquake3.x86_64" "-height" "1200" "-width" "1600" "-condebug" "-mode" "4")))
+
+(defcommand ardshin () ()
+  (uiop:launch-program (list "surf" "-S" "-I" "https://ibanking.ardshinbank.am:4443/nibanking/login")))
+(defcommand disroot () ()
+  (uiop:launch-program (list "surf" "https://webmail.disroot.org")))
+(defcommand proton () ()
+  (uiop:launch-program (list "surf" "-S" "https://proton.me")))
