905fed886ca7f5565a6409e6635ca1e9be950175
Artyom Bologov <git@aartaka.me>
2024-11-19 03:32:47 +0400

    Revert "config(:screenshot): Use screenshot-area, actually."
    This reverts commit e41a40b96eb6e69640c3541686db6f45872623a7.
    
    Because the old way, though more awkward, was giving me more agency
    over naming/formatting.

---
+++ screenshot.lisp
@@ -0,0 +1,11 @@
+(in-package :stumpwm-user)
+
+(defcommand timestamp-screenshot () ()
+  "Save a screenshot (as a timestamp-named .PNG) and open Gimp to edit it."
+  (let ((file-name (format nil (home "/Documents/screenshots/~X.png") (get-universal-time))))
+    (uiop/cl:ensure-directories-exist file-name)
+    (screenshot:screenshot-window file-name)
+    (uiop:launch-program (list "gimp" file-name))))
+
+(define-key *top-map* (kbd "s-s") "timestamp-screenshot")
+(define-key *top-map* (kbd "SunPrint_Screen") "timestamp-screenshot")
