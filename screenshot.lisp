95e46817b545311b8182f6201bba1fccff7d98c2
Artyom Bologov <git@aartaka.me>
2024-11-19 03:50:09 +0400

    screenshot(timestamp-): Directory ensure function portability.

--- screenshot.lisp
+++ screenshot.lisp
@@ -3,7 +3,7 @@
 (defcommand timestamp-screenshot () ()
   "Save a screenshot (as a timestamp-named .PNG) and open Gimp to edit it."
   (let ((file-name (format nil (home "/Documents/screenshots/~X.png") (get-universal-time))))
-    (uiop/cl:ensure-directories-exist file-name)
+    (ensure-directories-exist file-name)
     (screenshot:screenshot-window file-name)
     (uiop:launch-program (list "gimp" file-name))))
 
